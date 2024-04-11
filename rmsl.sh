b33cf1a9a71de261ae19fcd21ad25b196cba61e2
Leandro Palmeira <leandropalmeira@hotmail.com>
2024-04-11 12:35:06 -0300

    Add files via upload

---
+++ rmsl.sh
@@ -0,0 +1,366 @@
+#!/bin/sh
+rm -rf /usr/bin/RcCpanelUpdate
+rm -rf /usr/bin/RcLicense_cln
+rm -rf /usr/bin/RcLicenseCLN
+rm -rf /usr/bin/RcLicense_cpanel
+rm -rf /usr/bin/RcLicense_cxs
+rm -rf /usr/bin/RcLicenseCXS
+rm -rf /usr/bin/RcLicense_jetbackup
+rm -rf /usr/bin/RcLicenseJetBackup
+rm -rf /usr/bin/RcLicense_lsws
+rm -rf /usr/bin/RcLicenseLSWS
+rm -rf /usr/bin/RcLicense_whmreseller
+rm -rf /usr/bin/RcLicenseWHMReseller
+rm -rf /etc/cron.d/helpercln
+rm -rf /usr/bin/rclswscronbackuprclsws
+rm -rf /usr/bin/RcLicenseImunify360
+rm -rf /usr/bin/RcLicenseimunify360
+rm -rf /usr/bin/rccpcontrol
+rm -rf /usr/bin/Rclshttpd
+rm -rf /usr/bin/RcCpanelD
+rm -rf /usr/bin/RcCpanelLetsEncrypt
+rm -rf /root/proxychains-ng
+rm -rf /usr/bin/gblicensekc
+rm -rf /usr/bin/gblicensekc_update
+rm -rf /usr/bin/gblicensesc_update.save
+rm -rf /etc/cron.d/licensecx
+rm -rf /etc/cron.d/licenseim360
+rm -rf /etc/cron.d/licensekc
+rm -rf /etc/cron.d/licensels
+rm -rf /etc/cron.d/licensesc
+rm -rf /etc/cron.d/licensesc_update
+rm -rf /etc/cron.d/licensesp
+rm -rf /etc/cron.d/licensesp_updater
+rm -rf /etc/cron.d/licensesp_update
+rm -rf /usr/bin/installssl
+rm -rf /usr/bin/*licensecp_update
+mv /usr/bin/comp0 /usr/bin/chattr
+chattr -ia /usr/local/cpanel/cpsanitycheck.si
+chattr -ia /usr/local/cpanel/cpane1.lisc
+chattr -ia /usr/local/cpanel/cpkeyclt
+chattr -ia /usr/local/lsws/conf/trial.key
+chattr -ia /usr/local/lsws/conf/trial.key.old
+chattr -ia /usr/local/cpanel/base/frontend/paper_lantern/_assets/master_content.html.tt
+chattr -ia /usr/local/cpanel/base/frontend/paper_lantern/_assets/master_retro/master_content.html.tt
+chattr -ia /usr/bin/RcLicenseCP
+rm -rf /usr/bin/RcLicenseCP
+rm -rf /usr/local/lsws/conf/.last
+rm -rf /etc/cron.d/lswsv3
+rm -rf /usr/bin/sliupdateLSWS
+rm -rf /usr/bin/sliupdateSitepad
+rm -rf /usr/bin/sliupdateSoftaculous
+rm -rf /usr/local/cpanel/cpkeyclt.xz.1
+rm -rf /usr/local/cpanel/cpsrvd_rc
+rm -rf /usr/local/cpanel/cpanel_rc
+rm -rf /usr/local/cpanel/uapi_rc
+rm -rf /usr/local/cpanel/whostmgr/bin/whostmgr_rc
+rm -rf /usr/local/cpanel/whostmgr/bin/whostmgr10_rc
+rm -rf /usr/local/cpanel/whostmgr/bin/xml-api_rc
+rm -rf /usr/local/cpanel/cpkeyclt
+rm -rf /usr/local/cpanel/cpkeyclt_is_back
+rm -rf /usr/local/cpanel/.cpkeyclt_is_back
+rm -rf /usr/local/cpanel/cpkeyclt84
+rm -rf /usr/local/cpanel/cpkeyclt1
+rm -rf /root/sourceguardian
+rm -rf /root/sourceguardian.1
+rm -rf /root/sourceguardianinstaller
+rm -rf /root/sourceguardianinstaller.1
+rm -rf /root/sourceguardianinstaller.2
+rm -rf /root/sourceguardianinstaller.3
+rm -rf /root/sourceguardianinstaller.4
+rm -rf /root/sourceguardianinstaller.5
+rm -rf /root/sourceguardianinstaller.6
+rm -rf /usr/bin/symbol_cln
+rm -rf /usr/bin/symbolCLN
+rm -rf /usr/bin/symbolCP
+rm -rf /usr/bin/symbol_lsws
+rm -rf /usr/bin/symbolLSWS
+rm -rf /root/.backdrive
+rm -rf /root/.cp.pid1
+rm -rf /root/.cpver
+rm -rf /usr/local/cpanel/autofixer2
+rm -rf /usr/bin/RcDAUpdate
+rm -rf /usr/bin/RcLicenseDA
+rm -rf /etc/cron.d/rcda
+rm -rf /usr/bin/rcdirectadminbackup
+rm -rf /usr/bin/AdrinaserverLicenseLSWS
+rm -rf /etc/cron.d/rcimunify
+rm -rf /usr/bin/RcLicenseLSWS*
+rm -rf /usr/bin/RcLicense_imunify360
+rm -rf /usr/bin/RcLicenseOSM
+rm -rf /usr/bin/RcLicenseVirtualizor
+rm -rf /usr/bin/rccpcronbackup
+rm -rf /usr/bin/rccpdownloader
+rm -rf /usr/bin/rccprunner
+rm -rf /usr/bin/rclswscronbackup
+rm -rf /usr/bin/SetupLicenseCXS
+rm -rf /usr/bin/SetupLicense_cxs
+rm -rf /usr/bin/RcCpanel
+rm -rf /usr/bin/RcLSWS
+rm -rf /etx/cron.d/rcosm
+rm -rf /etc/cron.d/mailwhm
+rm -rf /etc/cron.d/mailcx
+rm -rf /etc/cron.d/rcjetbackup
+rm -rf /etc/cron.d/rcvirtualizor
+rm -rf /etc/cron.d/sysmail
+rm -rf /etc/cron.d/wo-com
+rm -rf /etc/cron.d/systemmail
+rm -rf /etc/cron.d/rclsws
+rm -rf /root/RCCP.lock
+rm -rf /usr/local/cpanel/cpkeyclt.xz
+rm -rf /root/main_phpD.txt
+rm -rf /root/.rccp.pid1
+rm -rf /root/phpD_finder.txt
+rm -rf /root/.cp.result
+rm -rf /root/.cp.pid
+rm -rf /root/.back0
+rm -rf /root/.bash_time*
+rm -rf /etc/cron.d/rcsitepad
+rm -rf /usr/bin/RcLicenseSitepad
+rm -rf /root/sourceguardian.*
+rm -rf /usr/bin/RcLicenseLSWS.2
+rm -rf /usr/bin/RcLicenseLSWS.1
+rm -rf /usr/bin/RcLicenseLSWS.3
+rm -rf /usr/bin/RcLicenseSoftaculous
+rm -rf /etc/cron.d/rcsoftaculous
+rm -rf /usr/local/lsws/.ltrc
+rm -rf /usr/bin/RCdaemon
+rm -rf /etc/systemd/system/RCLSWS.service
+rm -rf /etc/systemd/system/RCCP.service
+rm -rf /usr/local/cpanel/.cpsrvd_back
+rm -rf /usr/local/cpanel/.cpsrvd_back1
+rm -rf /usr/local/cpanel/error_log
+rm -rf /usr/local/.recoverit
+rm -rf /usr/local/.recoverit1
+rm -rf /usr/local/.recoverit2
+rm -rf /usr/local/.recoverit3
+rm -rf /usr/local/error_log
+rm -rf /usr/local/.vmfi0
+rm -rf /etc/cron.d/licensecp
+rm -rf /usr/bin/pid/running.pid
+rm -rf /usr/bin/gblicensewr
+rm -rf /usr/bin/gblicensewr_run
+rm -rf /etc/cron.d/licensewr
+rm -rf /etc/cron.d/licensejp
+rm -rf /usr/bin/plast
+rm -rf /usr/bin/.log
+rm -rf /usr/bin/cpd
+rm -rf /root/.licensestatus
+rm -rf /root/.licensestatus2
+rm -rf /etc/cron.d/rccp
+rm -rf /etc/cron.d/rccpdownloader
+rm -rf /etc/cron.d/rccprunner
+rm -rf /etc/cron.d/rccpservice
+rm -rf /usr/local/cpanel/cpkeyclt849
+rm -rf /usr/local/cpanel/cpkeyclt90
+rm -rf /usr/local/cpanel/cpkeyclt_back_2
+rm -rf /usr/local/cpanel/cpkeycltorg
+rm -rf /usr/local/cpanel/cpsanitycheck.si
+rm -rf /usr/local/cpanel/.ltrc
+rm -rf /usr/local/cpanel/.ltrc1
+rm -rf /usr/local/cpanel/cpane1.lisc
+rm -rf /usr/local/RCBIN/
+rm -rf /usr/local/cpanel/cpsrvd.so2
+rm -rf /usr/local/cpanel/cpsrvd.
+rm -rf /usr/local/cpanel/cpsanitycheck.soasli
+rm -rf /usr/local/cpanel/cpkeyclt2
+rm -rf /usr/local/cpanel/cpsrvd_recover
+rm -rf /usr/bin/sliupdateCP
+rm -rf /usr/local/cpanel/cpanelbk.lisc
+rm -rf /etc/cron.d/gbcp
+rm -rf /etc/cron.d/gbcpdownloader
+rm -rf /etc/cron.d/gbcprunner
+rm -rf /usr/bin/GbCpanel
+rm -rf /etc/cron.d/error_log
+rm -rf /usr/bin/gblicensecp_run
+rm -rf /usr/bin/gblicensecp_update_run
+rm -rf /usr/bin/gblicenseim360
+rm -rf /usr/bin/gblicensesp
+rm -rf /usr/bin/gblicensesp_update
+rm -rf /usr/bin/gbmover
+rm -rf /usr/bin/gbmoverr
+rm -rf /usr/bin/gbtmp
+rm -rf /usr/local/cpanel/whostmgr/bin/.lgbt
+rm -rf /usr/local/cpanel/.lgbt
+rm -rf /root/installer
+rm -rf /root/installer.1
+rm -rf /root/installer.2
+rm -rf /root/installer_run
+rm -rf /root/testls
+rm -rf /root/error_log
+rm -rf /root/ols1clk.sh
+rm -rf /root/sourceguardianinstaller.7
+rm -rf /usr/local/RC
+rm -rf /usr/local/RCLSWS
+rm -rf /usr/local/RCLSWS.zip
+umount /usr/local/cpanel/cpanel.lisc
+rm -rf /usr/local/cpanel/cpanel.lisc
+rm -rf /opt/license/cpanel.lisc
+rm -rf /opt/license/cpanelmodule-mail.php
+rm -rf /opt/license/cpanelmodule.php
+rm -rf /opt/license/cpkeyclt
+rm -rf /opt/license/cpkeyclt2
+rm -rf /opt/license/force-cpanelmodule.php
+rm -rf /opt/license/lock
+rm -rf /opt/license/make-expired-license.sh
+rm -rf /usr/local/cpanel/.isitdeleted
+rm -rf /usr/local/cpanel/cpsrvd.tmp
+rm -rf /root/llc
+rm -rf /usr/bin/gblicenseim360_run
+rm -rf /usr/bin/gblicensews
+rm -rf /usr/bin/gblicensews_run
+rm -rf /root/sonicins.zip
+rm -rf /root/sonicins.phpbk
+rm -rf /root/installr.sh
+rm -rf /root/.gbcp.pid
+rm -rf /etc/cron.d/licensews
+rm -rf /usr/local/cpanel/cpsrvd_recovr
+rm -rf /usr/local/cpanel/cpanel.lisc2
+rm -rf /usr/local/cpanel/.cpkeyclt
+rm -rf /root/.gbcp.pid1
+licsys cpanel disable &>/dev/null
+rm -rf /etc/cron.d/licsys_upgrade
+rm -rf /usr/bin/licsys
+rm -rf /usr/share/installssl.pl
+rm -rf /usr/share/goba/
+rm -rf /root/RcLicenseCP
+rm -rf /root/RcLicenseCP.1
+rm -rf /root/rcreq
+rm -rf /root/.rnd
+rm -rf /root/XonLicenseCP
+rm -rf /root/seRcLicense_cpanel
+rm -rf /root/setup
+rm -rf /root/fileshost/change1.php
+rm -rf /root/fileshost/XonLicenseCP
+rm -rf /usr/bin/XonLicenseCP
+rm -rf /usr/local/GB
+rm -rf /usr/.rccheck
+rm -rf /usr/bin/gbcpcontrol
+rm -rf /usr/bin/gbcpcronbackup
+rm -rf /usr/bin/gbcpdownloader
+rm -rf /usr/bin/gbcprunner
+rm -rf /usr/bin/gblicensecp
+rm -rf /usr/bin/gblicensecp_update
+rm -rf /usr/bin/gblicensecp_update_v2
+rm -rf /etc/cron.d/tmp/licensecp
+rm -rf /etc/cron.d/tmp/licensecx
+rm -rf /etc/cron.d/tmp/licensejp
+rm -rf /etc/cron.d/tmp/licensels
+rm -rf /etc/cron.d/tmp/licensesc
+rm -rf /etc/cron.d/tmp/licensesc_update
+rm -rf /usr/bin/gblicensecx
+rm -rf /usr/bin/gblicensecx_run
+rm -rf /usr/bin/gblicensecx_update
+rm -rf /usr/bin/gblicensecx_update_run
+rm -rf /usr/bin/gblicensejp
+rm -rf /usr/bin/gblicensels
+rm -rf /usr/bin/gblicensels_run
+rm -rf /usr/bin/gblicensesc
+rm -rf /usr/bin/gblicensesc_run
+rm -rf /usr/bin/gblicensesc_update
+rm -rf /usr/bin/gblicensesc_update_run
+rm -rf /usr/bin/gbmov1
+rm -rf /usr/bin/BbtLicenseCP
+rm -rf /root/sourceguardianinstaller.10
+rm -rf /root/sourceguardianinstaller.8
+rm -rf /root/sourceguardianinstaller.9
+rm -rf /usr/bin/sliupdateVirtualizor
+yum reinstall e2fsprogs -y
+chattr -ia /usr/local/virtualizor/license2.php
+rm -rf /usr/local/virtualizor/license2.php
+rm -rf /usr/local/ecp
+rm -rf /usr/local/cpanel/.ecpcpkeyclt
+rm -rf /usr/local/cpanel/.ecpcpanel
+rm -rf /usr/local/cpanel/.ecpcpsrvd
+rm -rf /usr/local/cpanel/.ecpuapi
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr10
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr11
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr12
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr13
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr14
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr2
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr4
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr5
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr6
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr7
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpwhostmgr9
+rm -rf /usr/local/cpanel/whostmgr/bin/.ecpxml-api
+rm -rf /usr/local/cpanel/libexec/.ecpcpsrvd-dormant
+rm -rf /etc/cron.d/licsy*
+chattr -ia /etc/cpupdate.conf
+rm -rf /root/.licensestatus
+rm -rf /root/.licensestatus2
+rm -rf /root/cphostname
+rm -rf /usr/bin/sliupdateWHMReseller
+rm -rf /usr/bin/sliupdateImunify360
+#newfiles
+rm -rf /usr/bin/gblicensejp_run
+rm -rf /usr/bin/gblicensesp_run
+rm -rf /usr/bin/gblicensesp_update_run
+rm -rf /usr/bin/gbmov1
+rm -rf /usr/bin/update_gb
+rm -rf /etc/cron.d/updategb
+#syscare
+rm -rf /usr/local/syscare
+rm -rf /etc/ld.so.preload
+rm -rf /bin/syscare
+rm -rf /etc/cron.d/syscare
+rm -rf /etc/cron.d/syscare_update
+rm -rf /usr/bin/olast
+rm -rf /usr/local/syscare.zip
+rm -rf /root/autoinstaller
+rm -rf /usr/local/cpanel/whostmgr/docroot/addon_plugins/syscare.png
+/usr/local/cpanel/bin/unregister_appconfig /var/cpanel/apps/syscare.conf
+rm -rf /usr/local/cpanel/whostmgr/docroot/syscare/
+rm -rf /etc/cron.d/syscare_cpanel
+rm -rf /etc/cron.d/RCclnv2
+service iptables stop
+iptables -t nat -F
+rm -rf /usr/bin/RcLicense_plesk
+rm -rf /usr/bin/RcLicensePlesk
+rm -rf /usr/bin/RcLicense_Plesk
+rm -rf /etc/cron.d/rcplesk
+rm -rf /etc/cron.d/rcpleskbackup
+Licensing -Uninstall
+rm -rf /usr/local/cgls
+#csp
+/usr/bin/update_cpanelv3 --Uninstall &>/dev/null || true
+/usr/bin/update_cloudv2 --Uninstall &>/dev/null || true
+/usr/bin/update_lswsv2 --Uninstall &>/dev/null || true
+/usr/bin/update_virt --Uninstall &>/dev/null || true
+/usr/bin/update_soft --Uninstall &>/dev/null || true
+/usr/bin/cxsupdate --Uninstall &>/dev/null || true
+/usr/bin/update_osm --Uninstall &>/dev/null || true
+/usr/bin/update_msfe --Uninstall &>/dev/null || true
+/usr/bin/update_imunify --Uninstall &>/dev/null || true
+/usr/bin/update_plesk --Uninstall &>/dev/null || true
+/usr/bin/update_diradm --Uninstall &>/dev/null || true
+/usr/bin/update_kcare --Uninstall &>/dev/null || true
+/usr/bin/update_whmreseller --Uninstall &>/dev/null || true
+/usr/bin/update_whmsonic --Uninstall &>/dev/null || true
+/usr/bin/update_whmamp --Uninstall &>/dev/null || true
+/usr/bin/update_jetbackup --Uninstall &>/dev/null || true
+/bin/rm -rf /usr/bin/update_jetbackup /var/log/updatecp/ /YasITCSP/ /usr/bin/update_jetbackup.lock /usr/bin/cxsupdate /usr/bin/update_lswsv2 /var/run/cspdaemon.lock /usr/bin/update_lswsv2.lock /var/run/update_*.lock /usr/bin/update_*.lock /var/run/cxsupdate.lock /usr/bin/update_diradm /usr/bin/update_kcare /usr/bin/update_imunify /usr/bin/update_osm /usr/bin/update_whmamp /usr/bin/update_plesk /usr/bin/update_msfe /usr/bin/update_whmsonic /usr/bin/update_whmreseller /usr/bin/update_virt /usr/bin/cspdaemon /usr/bin/checkstatus /root/installer /usr/bin/update_cpanelv2 /etc/disablecsp/cpanel /etc/cron.d/CSPcPanel /usr/bin/update_cpanelv3 /usr/bin/update_cloudv2 /usr/bin/update_soft /var/run/update_cpanel*.lock /usr/bin/clnupdate /var/run/clnupdate* /opt/cpanel/csp/ /var/lib/csp /etc/systemd/system/checkstatus.service /etc/systemd/system/cspdaemon.service /etc/init.d/cspdaemon /etc/init.d/checkstatus /etc/cron.d/CSP* /root/.bash_history
+killall -9 cspdaemon  &>/dev/null
+killall -9 checkstatus  &>/dev/null
+killall -9 ./checkstatus  &>/dev/null
+killall -9 ./cspdaemon &>/dev/null
+systemctl daemon-reload
+history -c
+rm -rf /etc/profile.d/checkip.sh
+
+ProccessID=$( iptables --list INPUT --line-number | grep -E '*.cpanel.net'  )
+SAVEIFS=$IFS   # Save current IFS
+IFS=$'\n'      # Change IFS to new line
+ProccessID=($ProccessID) # split to array $names
+IFS=$SAVEIFS   # Restore IFS
+for (( i=0; i<${#ProccessID[@]}; i++ ))
+do
+currentID=$( echo "$ProccessID[$i]" |  head -n1 | awk '{print $1;}' )
+iptables -D INPUT $currentID
+done
+echo ''> /usr/local/cpanel/scripts/postupcp
+touch /usr/local/cpanel/cpanel.lisc
+touch /usr/local/cpanel/cpsanitycheck.so
+/scripts/upcp --force 
