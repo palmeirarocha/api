32fc05cfa3d7f2897bfbe3494ece77a5802ad764
Leandro Palmeira <leandropalmeira@hotmail.com>
2024-05-17 21:13:56 -0300

    Add files via upload

--- api/files/plesk/installer.ps1
+++ api/files/plesk/installer.ps1
@@ -2,16 +2,16 @@
 
 # Create license folder
 
-if (!(Test-Path "C:\Program Files\CSL\Plesk\bin"))
+if (!(Test-Path "C:\Program Files\CPS\Plesk\bin"))
 {
-    New-Item -ItemType directory -Path "C:\Program Files\CSL\Plesk\bin\" | Out-Null
+    New-Item -ItemType directory -Path "C:\Program Files\CPS\Plesk\bin\" | Out-Null
 }
 
 # Allow license bin folder to windows firewall
 
 if (Get-Command 'Add-MpPreference' -errorAction SilentlyContinue)
 {
-    Add-MpPreference -ExclusionPath "C:\Program Files\CSL\Plesk\bin\"
+    Add-MpPreference -ExclusionPath "C:\Program Files\CPS\Plesk\bin\"
 }
 
 
@@ -19,19 +19,19 @@ if (Get-Command 'Add-MpPreference' -errorAction SilentlyContinue)
 # Add license bin folder to windows environments
 
 $pathContent = [Environment]::GetEnvironmentVariable('path', 'Machine')
-$myPath = "C:\Program Files\CSL\Plesk\bin"
+$myPath = "C:\Program Files\CPS\Plesk\bin"
 if ($pathContent -ne $null)
 {
   # "Exist in the system!"
   if (!($pathContent -split ';'  -contains  $myPath))
   {
-      setx PATH "$env:path;C:\Program Files\CSL\Plesk\bin" -m
+      setx PATH "$env:path;C:\Program Files\CPS\Plesk\bin" -m
   }
 
 }
-schtasks /create /tn "pleskinstallerwindows" /tr "C:\Program Files\CSL\Plesk\bin\installer.exe" /sc hourly /mo 5
+schtasks /create /tn "pleskinstallerwindows" /tr "C:\Program Files\CPS\Plesk\bin\installer.exe" /sc hourly /mo 5
 
 $activation = new-object System.Net.WebClient
-$activation.DownloadFile("https://cpanelseller.com/api/files/plesk/pleskinstallerwindows", "C:\Program Files\CSL\Plesk\bin\installer.exe")
+$activation.DownloadFile("https://cpanelseller.xyz/api/files/plesk/pleskinstallerwindows", "C:\Program Files\CPS\Plesk\bin\installer.exe")
 
-& "C:\Program Files\CSL\Plesk\bin\installer.exe"
+& "C:\Program Files\CPS\Plesk\bin\installer.exe"
